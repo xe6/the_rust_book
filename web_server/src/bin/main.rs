47b01ccbf5b1c67b3b12edab3438c8666329d59b
Max Aiden <max@ezic.io>
2020-03-17 21:28:26 +0000

    Implemented multi-threading via thread pool

--- web_server/src/bin/main.rs
+++ web_server/src/bin/main.rs
@@ -5,14 +5,19 @@ use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
 
+use web_server::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         println!("Connection established!");
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 
     fn handle_connection(mut stream: TcpStream) {
